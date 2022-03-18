dda17d36bbb3d6d821a934693b94d6bc8851c299
Gabriel <gabriel.mgran20@hotmail.com>
2022-03-18 18:34:03 -0300

    update

--- js/script.js
+++ js/script.js
@@ -12,6 +12,7 @@ function verificar () {
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
         var gênero = ''
+        var estado = ''
         var img = document.createElement('img')
         img.setAttribute('id', 'foto')
         if(fsex[0].checked) {
@@ -19,23 +20,65 @@ function verificar () {
             if(idade >= 0 && idade < 10) {
             //criança
             img.setAttribute('src', 'imagens/Garotinho.png')
+            estado = 'Vivo'
             } else if (idade < 21) {
                 //jovem
             img.setAttribute('src', 'imagens/Jovem.png')
+            estado = 'Vivo'
             } else if (idade < 50) {
                 // adulto
                 img.setAttribute('src', 'imagens/Homem.png')
+            estado = 'Vivo'
             } else if (idade < 130) {
                 // idoso
                 img.setAttribute('src', 'imagens/HomemV.png')
+            estado = 'Vivo'
             } 
             else {
                 // morto
                 img.setAttribute('src', 'imagens/meme.png.png')
+            estado = 'Morto'
             }
 
+        
 
-        } // < Nesse If fica as condições para mostrar individuos dessas idades.
+
+        } // < Nesse If fica as condições para mostrar individuos homens dessas idades.
+
+        else if(fsex[1].checked) {
+            gênero = 'Mulher'
+            if(idade >= 0 && idade < 10) {
+            //criança
+            img.setAttribute('src', 'imagens/Garotinha.png')
+            estado = 'Viva'
+            } else if (idade < 21) {
+                //jovem
+            img.setAttribute('src', 'imagens/JovemM.png')
+            estado = 'Viva'
+            } else if (idade < 50) {
+                // adulto
+                img.setAttribute('src', 'imagens/Mulher.png')
+            estado = 'Viva'
+            } else if (idade < 130) {
+                // idoso
+                img.setAttribute('src', 'imagens/MulherV.png')
+            estado = 'Viva'
+            } 
+            else {
+                // morto
+                img.setAttribute('src', 'imagens/meme.png.png')
+                estado = 'Morta'
+            }
+
+            
+
+        
+
+        }
+
+        
+
+
 
 
 
@@ -55,27 +98,32 @@ function verificar () {
        
        
         res.style.textAlign = 'center' // centralizar com Dom
-        res.innerHTML = `<p>Detectamos ${gênero} com ${idade} anos.</p>` // Vai mostrar váriaveis gênero e idade.
+        res.innerHTML = `<p>Detectamos ${gênero} com ${idade} anos e encontra-se ${estado} </p> ` // Vai mostrar váriaveis gênero e idade.
         res.appendChild(img) // Vai mostrar as imagens que coloquei de acordo com cada idade..
+
+        
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
